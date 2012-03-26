061f1bcfc415c3dea71143856dc0d0adbf283902
Jijun Gao <jjgao@clustertech.com>
2012-03-26 18:01:08 +0800

    Wiki system powered by webpy.

---
+++ c/challenge/readme.txt
@@ -0,0 +1,1 @@
+http://www.cprogramming.com/challenge.html
---
+++ trac/schema.sql
@@ -0,0 +1,8 @@
+use webpy;
+CREATE TABLE pages (
+    id INT AUTO_INCREMENT,
+    url TEXT,
+    title TEXT,
+    content TEXT,
+    primary key (id)
+);
