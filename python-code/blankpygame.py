d97155113ba21480e4949522a93db1b202c0d2d6
Jijun Gao <gaojijun.bit@gmail.com>
2012-03-28 22:59:16 +0800

    First pygame.

---
+++ python-code/blankpygame.py
@@ -0,0 +1,18 @@
+"""
+From http://inventwithpython.com/pygame/chapter2.html
+"""
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('hello world!')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
